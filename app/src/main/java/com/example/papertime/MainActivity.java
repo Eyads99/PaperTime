c1da80803c814d6679bd8ff5f4c6aa3c997ed915
eyads <eyadshaban99@gmail.com>
2019-05-31 23:11:30 +0200

    Slight changes

--- app/src/main/java/com/example/papertime/MainActivity.java
+++ app/src/main/java/com/example/papertime/MainActivity.java
@@ -8,9 +8,9 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.PaperTime.MESSAGE";// this is given to the intent
--- app/src/main/java/com/example/papertime/resultsPage.java
+++ app/src/main/java/com/example/papertime/resultsPage.java
@@ -35,7 +35,7 @@ public class resultsPage extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "© 2019 Eyad Shaban", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
